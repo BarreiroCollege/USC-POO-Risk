9557d78a256916c8b08e6e5d39abea038b5b061e
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-10-27 17:02:53 +0100

    Eliminar

--- src/gal/sdc/usc/risk/menu/Jugadores.java
+++ src/gal/sdc/usc/risk/menu/Jugadores.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gal.sdc.usc.risk.menu;
 
 
-public class Jugadores implements Comando{
-public void crearJugador(File fichero){
-    //Array de jugadores
-    ArrayList Jugadores= new ArrayList<>();
-    //Se lee el archivo y se guardan los nombres segun van leyendose
-    for (int i = 0; i < fichero.size; i++) {
-        Jugadores.add(i,fichero);
+public class Jugadores implements Comando {
+    public void crearJugador(File fichero) {
+        //Array de jugadores
+        ArrayList Jugadores = new ArrayList<>();
+        //Se lee el archivo y se guardan los nombres segun van leyendose
+        for (int i = 0; i < fichero.size; i++) {
+            Jugadores.add(i, fichero);
+        }
     }
-}
 }
 
