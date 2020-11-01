6641843572c9adfb508d7ae4927f6a7288bc899c
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-11-01 01:50:49 +0100

    Proguard

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -rf build/
 find -name "*.java" > sources.txt
 javac -d ./build @sources.txt
 cp -r res/* build/
 jar cvfm build/risk.jar build/META-INF/MANIFEST.MF -C build/ .
-java -jar build/risk.jar
+# java -jar build/risk.jar
+proguard/bin/proguard.sh @config.pro
