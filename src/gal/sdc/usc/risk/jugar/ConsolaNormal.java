c2d353fe2984670d19b0e20c228c9b311f10d56d
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-12-19 11:37:47 +0100

    Arreglo de consola normal

--- src/gal/sdc/usc/risk/jugar/ConsolaNormal.java
+++ src/gal/sdc/usc/risk/jugar/ConsolaNormal.java
@@ -1,8 +1,19 @@
 package gal.sdc.usc.risk.jugar;
 
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ConsolaNormal implements Consola {
+    private static final Scanner scanner;
+
+    static {
+        System.setIn(new FileInputStream(FileDescriptor.in));
+        scanner = new Scanner(System.in, StandardCharsets.ISO_8859_1.name()).useLocale(new Locale("es"));
+    }
+
     @Override
     public void imprimir(Object o) {
         System.out.print(o.toString());
@@ -15,10 +26,10 @@ public class ConsolaNormal implements Consola {
 
     @Override
     public String leer() {
-        Scanner input = new Scanner(System.in);
-        String comando = input.nextLine();
-        input.close();
+        return scanner.nextLine();
+    }
 
-        return comando;
+    public static void cerrar() {
+        scanner.close();
     }
 }
