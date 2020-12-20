b0581d3b28d56c6693fb483a0e22e9ed90142c31
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-12-20 17:45:56 +0100

    Arreglos menores

--- src/gal/sdc/usc/risk/util/Recursos.java
+++ src/gal/sdc/usc/risk/util/Recursos.java
@@ -1,16 +1,8 @@
 package gal.sdc.usc.risk.util;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 
 public class Recursos {
-    private static final File saida = new File("saida.txt");
-
     public static File get(String name) {
         return new File("res/" + name);
     }
