735ec3f2bb552326d297a947b4c86a94f9a116ca
Diego Barreiro <diego.barreiro.perez@gmail.com>
2020-12-21 11:12:25 +0100

    Cambios

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 rm -rf build/
 mkdir build/
 mkdir build/lib/
-find . -name "*.java" > build/sources.txt
-javac -d ./build -cp "*.jar" @build/sources.txt
+find . -type f -name '*.java' ! -name 'RiskGUI.java' ! -path "*gui*" > build/sources.txt
+javac -d ./build -cp "*.jar" -encoding UTF-8 @build/sources.txt
 cp -r res/* build/
 cp -r lib/* build/lib/
 jar cvfm build/risk.jar build/META-INF/MANIFEST.MF -C build/ .
--- src/gal/sdc/usc/risk/salida/ConsolaNormal.java
+++ src/gal/sdc/usc/risk/salida/ConsolaNormal.java
@@ -4,23 +4,34 @@ import gal.sdc.usc.risk.Risk;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ConsolaNormal implements Consola {
     private static final Scanner scanner;
+    private static final PrintStream ps;
 
     static {
         System.setIn(new FileInputStream(FileDescriptor.in));
         scanner = new Scanner(System.in,
                 Risk.netbeans ? StandardCharsets.ISO_8859_1.name() : StandardCharsets.UTF_8.name())
                 .useLocale(new Locale("es"));
+
+        PrintStream psTemp;
+        try {
+            psTemp = new PrintStream(System.out, true, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            psTemp = null;
+        }
+        ps = psTemp;
     }
 
     @Override
     public void imprimir(Object o) {
-        System.out.print(o.toString());
+        ps.print(o.toString());
     }
 
     @Override
@@ -35,5 +46,6 @@ public class ConsolaNormal implements Consola {
 
     public static void cerrar() {
         scanner.close();
+        ps.close();
     }
 }
